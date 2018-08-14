d0f970d058467e70315246006026cf7b3c42d352
panmingjie <1430001300@qq.com>
2018-08-14 22:25:38 +0800

    不再更新

--- src/main/java/expression/ExpressionDiscovery.java
+++ src/main/java/expression/ExpressionDiscovery.java
@@ -24,28 +24,8 @@ public class ExpressionDiscovery implements ExpressionParse {
 
 
     private void parse(String expression) {
-        if (expression.startsWith("excution")) {
-            String substring = expression.substring(9, expression.length() - 1);
-            String[] split = substring.split(" ");
-            returnName = split[0];
-            String left = split[1];
-            int i = left.lastIndexOf("(");
-            int i1 = left.lastIndexOf(")");
-            argsName = left.substring(i + 1, i1);
-            parseArgsName(argsName);
-            String names = left.substring(0, i);
-            int i2 = names.lastIndexOf(".");
-            methodName = names.substring(i2 + 1, names.length());
-        }
     }
 
-    private void parseArgsName(String argsName) {
-        if (argsName.equals("..")) {
-            argsName = "*";
-            return;
-        }
-        args = argsName.split(",");
-    }
 
     @Override
     public Boolean match(Method method) {
